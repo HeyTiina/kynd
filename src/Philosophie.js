fedf9822f8af14ad12adced53f1ae2b63454d58d
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-09-19 16:08:01 +0200

    we just started

--- src/Philosophie.js
+++ src/Philosophie.js
@@ -1,7 +1,13 @@
 import "./Philosophie.css";
+import PhilosophieBagel from "./img/philosophie-bagel.png";
+
 
 export default function Philosophie() {
   return (
- 
+    <section className="Philosophie">
+      <div className="container">
+        <img src={PhilosophieBagel} alt="Bagel" className=""/>
+      </div>
+    </section>
   );
 }
