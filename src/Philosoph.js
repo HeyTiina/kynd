9be7ca2da87cd9497e76a78ac2fb9f9005a8509e
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-09-20 17:51:03 +0200

    Philosophie part done, easy

--- src/Philosoph.js
+++ src/Philosoph.js
@@ -4,9 +4,22 @@ import PhilosophieBagel from "./img/PhilosophieBagel.webp";
 
 export default function Philosoph() {
   return (
-    <section className="Philosoph">
-      <div className="container">
-        <img src={PhilosophieBagel} alt="Bagel" className=""/>
+    <section className="philosoph">
+      <div className="grid-container-philosoph">
+        <img src={PhilosophieBagel} alt="Bagel" className="" />
+        <div className="phisoloph-description">
+          <h1>UNSERE PHILOSOPHIE</h1>
+          <h2>Be Kynd whenever possible. It is always possible</h2>
+          <p>
+            Bei Kynd geht es darum, den Menschen durch eine ausgewogene
+            Ernährung näherzukommen und ein nachhaltiges Kynd-Erlebnis zu
+            schaffen. Für uns steht Kyndness für Menschlichkeit, Nachhaltigkeit,
+            Selbstliebe, Lokalität und Gesundheit. Jede Handlung von Kynd
+            basiert auf dem Grundsatz: Sei Kynd, wann immer es möglich ist. Es
+            ist immer möglich."
+          </p>
+          <a>ÜBER UNS</a>
+        </div>
       </div>
     </section>
   );
