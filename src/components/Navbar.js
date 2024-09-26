760dca03d39887183ac83416cd7c5d8e02ff186b
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-09-26 11:56:52 +0200

    Navbar
    logo img added and working with link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-
+import Logo from "../img/kynd-logo.png";
 
 const Navbar = () => {
   return (
-      <nav className="navbar">
-        <ul className="nav-links">
-          <li>
-            <Link to="/MenuScreen">MENÜ</Link>
-          </li>
-          <li>
-            <Link to="/CateringScreen">CATERING & EVENTS</Link>
-          </li>
-          <li>
-            <Link to="/KontaktScreen">KONTAKT</Link>
-          </li>
-          <li>
-            <Link to="/UberUnsScreen">ÜBER UNS</Link>
-          </li>
-        </ul>
-      </nav>
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li>
+          <Link to="/">
+            <img src={Logo} alt="Logo" className="logo" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/MenuScreen">MENÜ</Link>
+        </li>
+        <li>
+          <Link to="/CateringScreen">CATERING & EVENTS</Link>
+        </li>
+        <li>
+          <Link to="/KontaktScreen">KONTAKT</Link>
+        </li>
+        <li>
+          <Link to="/UberUnsScreen">ÜBER UNS</Link>
+        </li>
+      </ul>
+    </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
