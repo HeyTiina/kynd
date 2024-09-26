c8f07f1dabf93ed54b8f32561e7a8a81abde5109
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-09-26 15:19:51 +0200

    trying to do a slider............

--- src/cateringScreen/Carrusel.js
+++ src/cateringScreen/Carrusel.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Review1 from "./img/review1.png";
 import Review2 from "./img/review2.png";
 import Review3 from "./img/review3.png";
+import "./Carrusel.css";
+import useInterval from "use-interval";
 
 export const Carrusel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const images = [Review1, Review2, Review3];
+
+  useInterval(() => {
+    setCurrentIndex((currentIndex + 1) % images.length);
+  }, 2000); // cambiar cada 5 segundos
+
+  const handlePrevClick = () => {
+    setCurrentIndex((currentIndex - 1 + images.length) % images.length);
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((currentIndex + 1) % images.length);
+  };
+
   return (
     <div>
-      <div class="carrusel-container">
-        <div class="carrusel">
-          <div class="carrusel-item">
-            <img src={Review1} alt="Review1" className="Review1" />{" "}
-          </div>
-          <div class="carrusel-item">
-            <img src={Review2} alt="Review2" className="Review2" />{" "}
-          </div>
-          <div class="carrusel-item">
-            <img src={Review3} alt="Review3" className="Review3" />{" "}
+      <div className="carrusel-container">
+        <div className="carrusel">
+          <div className="carrusel-item.active">
+            <img
+              src={images[currentIndex]}
+              alt={`Review ${currentIndex + 1}`}
+            />
           </div>
         </div>
-        <div class="carrusel-controls">
-          <button class="carrusel-prev">Anterior</button>
-          <button class="carrusel-next">Siguiente</button>
+        <div className="carrusel-controls">
+          <button className="carrusel-prev" onClick={handlePrevClick}>
+            Anterior
+          </button>
+          <button className="carrusel-next" onClick={handleNextClick}>
+            Siguiente
+          </button>
         </div>
       </div>
     </div>
