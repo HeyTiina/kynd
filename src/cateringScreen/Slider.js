e790d15287871a4b2286796cf37d7174b445b6f7
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-09-28 19:18:17 +0200

    styles to slider.css

--- src/cateringScreen/Slider.js
+++ src/cateringScreen/Slider.js
@@ -1,87 +1,53 @@
 import { useState } from "react";
+import "./Slider.css";
+
+const slideStyles = {
+  width: "100%",
+  height: "100%",
+  borderRadius: "10px",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+};
 
 const ImageSlider = ({ slides }) => {
-
-    const slideStyles = {
-      width: "100%",
-      height: "100%",
-      borderRadius: "10px",
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-    };
-
-    const rightArrowStyles = {
-      position: "absolute",
-      top: "50%",
-      transform: "translate(0, -50%)",
-      right: "32px",
-      fontSize: "45px",
-      color: "#fff",
-      zIndex: 1,
-      cursor: "pointer",
-    };
-
-    const leftArrowStyles = {
-      position: "absolute",
-      top: "50%",
-      transform: "translate(0, -50%)",
-      left: "32px",
-      fontSize: "45px",
-      color: "#fff",
-      zIndex: 1,
-      cursor: "pointer",
-    };
-
-    const sliderStyles = {
-      position: "relative",
-      height: "100%",
-    };
-
-    const dotsContainerStyles = {
-      display: "flex",
-      justifyContent: "center",
-    };
-
-    const dotStyle = {
-      margin: "0 3px",
-      cursor: "pointer",
-      fontSize: "20px",
-    };
-
   const [currentIndex, setCurrentIndex] = useState(0);
+
   const goToPrevious = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
+
   const goToNext = () => {
     const isLastSlide = currentIndex === slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
   };
+
   const goToSlide = (slideIndex) => {
     setCurrentIndex(slideIndex);
   };
+
   const slideStylesWidthBackground = {
     ...slideStyles,
     backgroundImage: `url(${slides[currentIndex].url})`,
   };
 
   return (
-    <div style={sliderStyles}>
+    <div className="slider">
       <div>
-        <div onClick={goToPrevious} style={leftArrowStyles}>
+        <div onClick={goToPrevious} className="left-arrow">
           ❰
         </div>
-        <div onClick={goToNext} style={rightArrowStyles}>
+        <div onClick={goToNext} className="right-arrow">
           ❱
         </div>
       </div>
       <div style={slideStylesWidthBackground}></div>
-      <div style={dotsContainerStyles}>
+      <div className="dots-container">
         {slides.map((slide, slideIndex) => (
           <div
-            style={dotStyle}
+            className="dot"
             key={slideIndex}
             onClick={() => goToSlide(slideIndex)}
           >
