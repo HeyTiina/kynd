1c4ca08435a7963b3977538faf99da536ec46fd1
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-09-28 20:08:11 +0200

    bagel decoration added

--- src/cateringScreen/CateringScreen.js
+++ src/cateringScreen/CateringScreen.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./CateringScreen.css";
+
 import Video from "./img/Video.webp";
+import Bagel from "../main/img/bagel.png";
+
 import LokalesWasser from "./img/LokalesWasser-logo.png";
 import Plaza from "./img/Plaza-Logo.png";
 import Kantonal from "./img/Zurcher-Kantonalbank-Logo.png";
-import Slider from "./Slider";
 
+import Slider from "./Slider";
 import Review1 from "./img/review1.png";
 import Review2 from "./img/review2.png";
 import Review3 from "./img/review3.png";
@@ -46,6 +49,7 @@ const CateringScreen = () => {
         </div>
       </div>
       <div className="Video">
+        <img src={Bagel} alt="Bagel" className="bagel-cateringScreen" />
         <img src={Video} alt="Video" /> <br />
         <button>JETZT RESERVIEREN</button>
       </div>
