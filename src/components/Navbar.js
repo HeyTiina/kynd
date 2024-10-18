2dc2797ba01804378ba0e8b6a4acf7cf0cd16603
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-10-18 18:32:09 +0200

    nav bar with the bootstrap template

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,56 @@
 import React from "react";
-import "./Navbar.css";
+import "./Navbar.css"; // Esto es opcional si quieres mantener estilos personalizados
 import { Link } from "react-router-dom";
 import Logo from "./kynd-logo.png";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul className="nav-links">
-        <li>
-          <Link to="/">
-            <img src={Logo} alt="Logo" className="logo" />
-          </Link>
-        </li>
-        <li>
-          <Link to="/MenuScreen">MENÜ</Link>
-        </li>
-        <li>
-          <Link to="/CateringScreen">CATERING & EVENTS</Link>
-        </li>
-        <li>
-          <Link to="/KontaktScreen">KONTAKT</Link>
-        </li>
-        <li>
-          <Link to="/UberUnsScreen">ÜBER UNS</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        {/* Logo en el navbar */}
+        <Link className="navbar-brand" to="/">
+          <img src={Logo} alt="Logo" className="logo" />
+        </Link>
+
+        {/* Botón de colapsado para móviles */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Enlaces del navbar */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/MenuScreen">
+                MENÜ
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/CateringScreen">
+                CATERING & EVENTS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/KontaktScreen">
+                KONTAKT
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/UberUnsScreen">
+                ÜBER UNS
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
