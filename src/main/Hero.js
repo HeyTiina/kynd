851102b955287e41e06009b906138e01ee2d0a0a
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-10-21 15:18:40 +0200

    hero img as background

--- src/main/Hero.js
+++ src/main/Hero.js
@@ -1,14 +1,12 @@
 import "./Hero.css";
-import Bagel from "./img/bagel.png";
 import Catering from "./Catering";
 import Menu from "./Menu";
 import Philosoph from "./Philosoph";
 
 export default function Hero() {
   return (
-    <section className="hero">
-      <div>
-        <img src={Bagel} alt="Bagel" className="bagel" />
+    <section>
+      <div className="hero">
         <div className="title">
           <h1>KYND</h1>
           <h2>We serve kyndness</h2>
