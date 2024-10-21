d248c5a349db01ea3aa022c8015c87d9c6846cf4
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-10-21 15:13:23 +0200

    img as background img

--- src/main/Catering.js
+++ src/main/Catering.js
@@ -3,15 +3,12 @@ import Bagel from "./img/bagel.png";
 
 export default function Catering() {
   return (
-    <section className="Catering">
-      <div className="container-catering">
-        <img src={Bagel} alt="Bagel" className="catering-bagel" />
+    <section className="catering">
         <div className="catering-title">
           <h1>CATERING & EVENTS</h1>
         
           <a href="###">KONTAKTIEREN SIE UNS</a>
         </div>
-      </div>
     </section>
   );
 }
