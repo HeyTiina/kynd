ed9f6fc3a1364e5ecf6d381cc2747228ff8ccce3
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-10-24 18:41:04 +0200

    photos for uber uns added

--- src/uberUnsScreen/lokal.js
+++ src/uberUnsScreen/lokal.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import "./lokal.css";
 
 const lokal = () => {
   return (
-    <div>lokal</div>
-  )
+    <div className="lokal">
+      <h1>Lokal</h1>
+      <p>
+        {" "}
+        Bei Kynd glauben wir an nachhaltige, gesunde und regionale Lebensmittel.
+        Wir sind stolz darauf, unseren Kunden die besten Bagel-Spezialitäten mit
+        frischen, hochwertigen Zutaten anzubieten. Der Großteil unserer Produkte
+        stammt aus biologischem Anbau und wir kooperieren nahezu ausschließlich
+        mit lokalen Partnern.
+      </p>
+      <img></img>
+    </div>
+  );
 }
 
 export default lokal
