88c288bff55527e802a93bb95da0c5b916164e86
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-10-24 17:43:31 +0200

    uber uns screen started

--- src/uberUnsScreen/UberUnsScreen.js
+++ src/uberUnsScreen/UberUnsScreen.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import "./UberUnsScreen.css";
 
 const UberUnsScreen = () => {
   return (
-    <div>UberUnsScreen</div>
-  )
+    <div>
+      <h1>UNSER BAGELFAHRRAD</h1>
+      <p>
+        Kynd ist ein mobiles Bagelfahrrad, das sich auf frische, gesunde und
+        regionale Bagel spezialisiert. Wir verwenden nur hochwertige, lokale
+        Zutaten, um jeden Bissen zum Genuss zu machen. Mit unserem Fahrrad
+        kommen wir direkt zu Ihnen und bieten die besten Bagelkreationen der
+        Region an – nachhaltig, lecker und bequem.
+      </p>
+    </div>
+  );
 }
 
 export default UberUnsScreen
