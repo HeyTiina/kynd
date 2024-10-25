308442a7631652f85e8fb1afb98678f31ee68c8d
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-10-25 17:09:05 +0200

    text and img added. style?

--- src/uberUnsScreen/lokal.js
+++ src/uberUnsScreen/lokal.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import "./lokal.css";
 
+import Bio from "./img/bio.png";
+import EatLocal from "./img/local.png";
+
+
 const lokal = () => {
   return (
     <div className="lokal">
-      <h1>Lokal</h1>
-      <p>
-        {" "}
-        Bei Kynd glauben wir an nachhaltige, gesunde und regionale Lebensmittel.
-        Wir sind stolz darauf, unseren Kunden die besten Bagel-Spezialitäten mit
-        frischen, hochwertigen Zutaten anzubieten. Der Großteil unserer Produkte
-        stammt aus biologischem Anbau und wir kooperieren nahezu ausschließlich
-        mit lokalen Partnern.
-      </p>
-      <img></img>
+      <div className='container-lokal'>
+        <div className="title-lokal">
+          <h1>Lokal</h1>
+          <p>
+            {" "}
+            Bei Kynd glauben wir an nachhaltige, gesunde und regionale
+            Lebensmittel. Wir sind stolz darauf, unseren Kunden die besten
+            Bagel-Spezialitäten mit frischen, hochwertigen Zutaten anzubieten.
+            Der Großteil unserer Produkte stammt aus biologischem Anbau und wir
+            kooperieren nahezu ausschließlich mit lokalen Partnern.
+          </p>
+        </div>
+        <div className="stickers">
+          <img src={Bio} alt="bio"></img>
+          <img src={EatLocal} alt="bio"></img>
+        </div>
+      </div>
     </div>
   );
 }
