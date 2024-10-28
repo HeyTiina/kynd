f4a2f451f507f6db6f4c5e720d3fb626ea04220c
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-10-28 15:06:20 +0100

    starting with the background img of tasks

--- src/uberUnsScreen/Tasks.js
+++ src/uberUnsScreen/Tasks.js
@@ -4,8 +4,17 @@ import "./Tasks.css";
 
 const Tasks = () => {
   return (
-    <div>Tasks</div>
-  )
+    <div className="tasks">
+      <div className="tasks-title">
+        <h1>Tasks</h1>
+        <p>
+          Kynd Tasks fördert positive Aktionen der Freundlichkeit und
+          Nachhaltigkeit durch anpassbare Aufgaben bei Veranstaltungen, die
+          kostenlos an dem KYND-Automaten erhältlich sind.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Tasks
