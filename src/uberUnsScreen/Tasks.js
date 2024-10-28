52dad46a085eab546eb21c7d7e982857778ff143
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-10-28 21:23:58 +0100

    tasks bubbles done

--- src/uberUnsScreen/Tasks.js
+++ src/uberUnsScreen/Tasks.js
@@ -1,20 +1,24 @@
-import React from 'react'
+import React from "react";
 import "./Tasks.css";
 
+import Bubble from "./img/bubble.png";
 
 const Tasks = () => {
   return (
     <div className="tasks">
+      <img src={Bubble} alt="bubble" className="bubble1"></img>
+      <img src={Bubble} alt="bubble" className="bubble2"></img>
+      <img src={Bubble} alt="bubble" className="bubble3"></img>
       <div className="tasks-title">
         <h1>Tasks</h1>
         <p>
           Kynd Tasks fördert positive Aktionen der Freundlichkeit und
           Nachhaltigkeit durch anpassbare Aufgaben bei Veranstaltungen, die
           kostenlos an dem KYND-Automaten erhältlich sind.
-        </p>
+        </p>{" "}
       </div>
     </div>
   );
-}
+};
 
-export default Tasks
+export default Tasks;
