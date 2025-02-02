1d38eaa2777c1d939eece1c20f97360f10dfcaeb
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2025-02-02 17:29:03 +0100

    owner finished

--- src/uberUnsScreen/Owner.js
+++ src/uberUnsScreen/Owner.js
@@ -1,24 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./Owner.css";
 
 import Raphael from "./img/Raphael.jpg";
 
-
 const Guy = () => {
   return (
-    <div className='guy'>
-      <h1>Lerne Raphael kennen</h1>
-      <p>
-        Wir präsentieren Raphael, das Herz und die Seele hinter unseren
-        exquisiten Bagels. Getrieben von seiner Leidenschaft für gutes Essen und
-        einen gesunden Lebensstil beschloss er, ein Unternehmen zu gründen, das
-        beide Welten vereint. Jeder Bagel, den wir anbieten, wird aus frischen,
-        hochwertigen Zutaten hergestellt und spiegelt sein Engagement wider.
-        Komm und genieße einen einzigartigen Geschmack, den nur Raphael bieten
-        kann!
-      </p>
-      <img src={Raphael} alt='Raphael' className='Raphael'></img>
+    <div className="owner">
+      <div className="container-owner">
+        <div className="title-owner">
+          <h1>Lerne Raphael kennen</h1>
+          <p>
+            Wir präsentieren Raphael, das Herz und die Seele hinter unseren
+            exquisiten Bagels. Getrieben von seiner Leidenschaft für gutes Essen
+            und einen gesunden Lebensstil beschloss er, ein Unternehmen zu
+            gründen, das beide Welten vereint. Jeder Bagel, den wir anbieten,
+            wird aus frischen, hochwertigen Zutaten hergestellt und spiegelt
+            sein Engagement wider. Komm und genieße einen einzigartigen
+            Geschmack, den nur Raphael bieten kann!
+          </p>
+        </div>
+        <img src={Raphael} alt="Raphael" className="Raphael"></img>
+      </div>
     </div>
   );
-}
+};
 
-export default Guy
+export default Guy;
