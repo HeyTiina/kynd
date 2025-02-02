9498455e17a4b10a0dde2a4bc169969523cdacb6
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2025-02-02 17:54:54 +0100

    KIND Tasks as grid and with one img of bubbles

--- src/uberUnsScreen/Tasks.js
+++ src/uberUnsScreen/Tasks.js
@@ -1,21 +1,25 @@
 import React from "react";
 import "./Tasks.css";
 
-import Bubble from "./img/bubble.png";
+import Bubbles from "./img/bubbles.png";
+import buMachine from "./img/buMachine.png";
 
 const Tasks = () => {
   return (
     <div className="tasks">
-      <img src={Bubble} alt="bubble" className="bubble1"></img>
-      <img src={Bubble} alt="bubble" className="bubble2"></img>
-      <img src={Bubble} alt="bubble" className="bubble3"></img>
-      <div className="tasks-title">
-        <h1>KYND Tasks</h1>
-        <p>
-          Kynd Tasks fördert positive Aktionen der Freundlichkeit und
-          Nachhaltigkeit durch anpassbare Aufgaben bei Veranstaltungen, die
-          kostenlos an dem KYND-Automaten erhältlich sind.
-        </p>{" "}
+      <div className="container-tasks">
+        <img src={buMachine} alt="bubble machine" className="buMachine"></img>
+        <div className="tasks-title">
+          <h1>KYND Tasks</h1>
+          <p>
+            Kynd Tasks fördert positive Aktionen der Freundlichkeit und
+            Nachhaltigkeit durch anpassbare Aufgaben bei Veranstaltungen, die
+            kostenlos an dem KYND-Automaten erhältlich sind.
+          </p>{" "}
+        </div>
+        <div className="bubbles">
+          <img src={Bubbles} alt="bubbles" className="bubbles"></img>
+        </div>
       </div>
     </div>
   );
