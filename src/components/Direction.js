64617139d81f3637f837e843bcb10c554c991a47
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2025-02-04 20:04:37 +0100

    directions js modified
    @media (max-width: 426px) {
    .direction {
        font-size: 16px;
    }
    }

--- src/components/Direction.js
+++ src/components/Direction.js
@@ -4,9 +4,10 @@ export default function Direction() {
   return (
     <section className="direction">
       <div className="direction">
-        Zürich Altstetten Bahnhof, 8048 Zúrich | Montag- Freitag 11:00 - 14:00
+        Zürich Altstetten Bahnhof, 8048 Zürich | Montag- Freitag 11:00 - 14:00
         Uhr
       </div>
+  
     </section>
   );
 }
